ad07dfcfd936eaccb310621690597bbf99f14516
Jakub Wysowski <kbwysowski@gmail.com>
2022-02-26 17:23:25 +0100

    Resolve power source problem. Works properly with gateway

--- src/main.cpp
+++ src/main.cpp
@@ -7,60 +7,65 @@
 
 #include "painlessMesh.h"
 
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
-#define   MESH_PORT       5555
+const char *mesh_prefix = "esp_mesh";
+const char *mesh_password = "123456789";
+const int mesh_port = 5555;
 
-Scheduler userScheduler; // to control your personal task
+Scheduler user_scheduler; // to control your personal task
 painlessMesh mesh;
 
-// User stub
-void sendMessage();  // Prototype so PlatformIO doesn't complain
+//Scheduler
+void send_message();
+Task task_send_message(TASK_SECOND * 1, TASK_FOREVER, &send_message);
 
-Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
-
-void sendMessage() {
-	String msg = "Hi from node 1, id: ";
-
-	msg += mesh.getNodeId();
-	mesh.sendBroadcast(msg);
-	taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
-}
-
-// Needed for painless library
-void receivedCallback(uint32_t from, String &msg) {
-	Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-}
-
-void newConnectionCallback(uint32_t nodeId) {
-	Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
-
-void changedConnectionCallback() {
-	Serial.printf("Changed connections\n");
-}
-
-void nodeTimeAdjustedCallback(int32_t offset) {
-	Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
+//Mesh callbacks
+void received_callback(uint32_t from, String &msg);
+void new_connection_callback(uint32_t nodeId);
+void changed_connection_callback();
+void node_time_adjusted_callback(int32_t offset);
 
 void setup() {
 	Serial.begin(9600);
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-	mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
+	mesh.setDebugMsgTypes(ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE);   // all types on
+	// mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
 
-	mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
-	mesh.onReceive(&receivedCallback);
-	mesh.onNewConnection(&newConnectionCallback);
-	mesh.onChangedConnections(&changedConnectionCallback);
-	mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+	mesh.init(mesh_prefix, mesh_password, &user_scheduler, mesh_port);
+	mesh.onReceive(&received_callback);
+	mesh.onNewConnection(&new_connection_callback);
+	mesh.onChangedConnections(&changed_connection_callback);
+	mesh.onNodeTimeAdjusted(&node_time_adjusted_callback);
+	mesh.setContainsRoot(true);
 
-	userScheduler.addTask(taskSendMessage);
-	taskSendMessage.enable();
+	user_scheduler.addTask(task_send_message);
+	task_send_message.enable();
 }
 
 void loop() {
 	// it will run the user scheduler as well
 	mesh.update();
+}
+
+void send_message() {
+	String msg = "Hi from node 2, id: ";
+
+	msg += mesh.getNodeId();
+	mesh.sendBroadcast(msg);
+	task_send_message.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
+}
+
+void received_callback(uint32_t from, String &msg) {
+	Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void new_connection_callback(uint32_t nodeId) {
+	Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changed_connection_callback() {
+	Serial.printf("Changed connections\n");
+}
+
+void node_time_adjusted_callback(int32_t offset) {
+	Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
